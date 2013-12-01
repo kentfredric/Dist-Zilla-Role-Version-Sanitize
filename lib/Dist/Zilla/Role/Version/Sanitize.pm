52819484d703cb8405199657fe0401a414173fe6
Kent Fredric <kentfredric@gmail.com>
2013-12-01 23:03:50 +1300

    Build results of 9d0f07a (on master)

---
+++ lib/Dist/Zilla/Role/Version/Sanitize.pm
@@ -0,0 +1,131 @@
+use strict;
+use warnings;
+
+package Dist::Zilla::Role::Version::Sanitize;
+BEGIN {
+  $Dist::Zilla::Role::Version::Sanitize::AUTHORITY = 'cpan:KENTNL';
+}
+{
+  $Dist::Zilla::Role::Version::Sanitize::VERSION = '0.001000';
+}
+
+# ABSTRACT: Sanitize a version from a plugin
+
+use Moose::Role;
+use Moose::Util::TypeConstraints qw(enum);
+
+sub _normalize_normal {
+  my ( $self, $orig ) = @_;
+  require version;
+  return version->parse($orig)->normal;
+}
+
+sub _normalize_normal_3 {
+  my ( $self, $orig ) = @_;
+  require version;
+  my $v = version->parse($orig)->normal;
+  $v =~ s/\Av//msx;
+  if ( $v !~ /\A\d+[.]\d+[.]\d+/msx ) {
+    require Carp;
+    return Carp::croak("Normalised string $v does not have a minimum of 3 parts");
+  }
+  return $v;
+}
+
+sub _normalize_numify {
+  my ( $self, $orig ) = @_;
+  require version;
+  my $version = version->parse($orig)->numify;
+  if ( $version =~ /(\A\d+)[.](.*$)/msx ) {
+    my ( $sig, $mantissa ) = ( $1, $2 );
+    my $got  = length $mantissa;
+    my $want = $self->mantissa;
+    if ( $got == $want ) {
+      return $version;
+    }
+    $self->log( [ 'MANTISSA LENGTH != EXPECTED: WANTED %s, GOT %s, CORRECTING', $want, $got ] );
+    if ( $want < $got ) {
+      my $newman = substr $mantissa, 0, $want;
+      return $sig . q[.] . $newman;
+    }
+    my $need = $want - $got;
+    return $sig . q[.] . $mantissa . ( q[0] x $need );
+  }
+  require Carp;
+  return Carp::croak(qq[Could not parse mantissa from numified version $version]);
+}
+
+my %normal_forms = (
+  normal   => '_normalize_normal',
+  normal_3 => '_normalize_normal_3',
+  numify   => '_normalize_numify',
+);
+
+has normal_form => (
+  is => ro =>,
+  isa => enum( [ keys %normal_forms ] ),
+  is => 'ro',
+  lazy    => 1,
+  default => sub { return 'numify' },
+);
+has mantissa => (
+  is      => ro =>,
+  isa     => 'Int',
+  is      => 'ro',
+  lazy    => 1,
+  default => sub {
+    ## no critic (ProhibitMagicNumbers
+    return 6;
+  },
+);
+
+around provide_version => sub {
+  my ( $orig, $self, @args ) = @_;
+  my $v      = $orig->( $self, @args );
+  my $method = $normal_forms{ $self->normal_form };
+  my $fixed  = $self->$method($v);
+  $self->log("Version normalised from $v to $fixed");
+  return $fixed;
+};
+
+around dump_config => sub {
+  my ( $orig, $self, @args ) = @_;
+  my $config = $orig->( $self, @args );
+  my $own_config = {
+    normal_form => $self->normal_form,
+    mantissa    => $self->mantissa,
+  };
+  $config->{ q[] . __PACKAGE__ } = $own_config;
+  return $config;
+};
+
+no Moose::Role;
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::Role::Version::Sanitize - Sanitize a version from a plugin
+
+=head1 VERSION
+
+version 0.001000
+
+=head1 AUTHOR
+
+Kent Fredric <kentfredric@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Kent Fredric <kentfredric@gmail.com>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
