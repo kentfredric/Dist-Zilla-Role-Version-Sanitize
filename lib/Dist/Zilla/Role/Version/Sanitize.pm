891f949128f4757ed2ce8143d6f11881e81a6fd1
Kent Fredric <kentfredric@gmail.com>
2013-12-01 22:05:47 +1300

    Build results of c035e9d (on master)

--- lib/Dist/Zilla/Role/Version/Sanitize.pm
+++ lib/Dist/Zilla/Role/Version/Sanitize.pm
@@ -20,6 +20,17 @@ sub _normalize_normal {
   return version->parse($orig)->normal;
 }
 
+sub _normalize_normal_3 {
+  my ( $self, $orig ) = @_;
+  require version;
+  my $v = version->parse($orig)->normal;
+  $v =~ s/^v//;
+  if ( $v !~ /^\d+[.]\d+[.]\d+/ ) {
+    die "Normalised string $v does not have a minimum of 3 parts";
+  }
+  return $v;
+}
+
 sub _normalize_numify {
   my ( $self, $orig ) = @_;
   require version;
@@ -31,7 +42,7 @@ sub _normalize_numify {
     if ( $got == $want ) {
       return $version;
     }
-    $self->log( [ 'MANTISSA LENGTH != EXPECTED: WANTED %s, GOT %s, CORRECTING', $want, length $got ] );
+    $self->log( [ 'MANTISSA LENGTH != EXPECTED: WANTED %s, GOT %s, CORRECTING', $want, $got ] );
     if ( $want < $got ) {
       my $newman = substr( $mantissa, 0, $want );
       return $sig . q[.] . $newman;
@@ -47,8 +58,9 @@ sub _normalize_numify {
 }
 
 my %normal_forms = (
-  normal => '_normalize_normal',
-  numify => '_normalize_numify',
+  normal   => '_normalize_normal',
+  normal_3 => '_normalize_normal_3',
+  numify   => '_normalize_numify',
 );
 
 has normal_form => ( is => ro =>, isa => enum( [ keys %normal_forms ] ), is => 'ro', lazy => 1, default => sub { 'numify' } );
@@ -59,7 +71,19 @@ around provide_version => sub {
   my $v      = $orig->( $self, @args );
   my $method = $normal_forms{ $self->normal_form };
   my $fixed  = $self->$method($v);
-  $self->log("Version normalised to $v");
+  $self->log("Version normalised from $v to $fixed");
+  return $fixed;
+};
+
+around dump_config => sub {
+  my ( $orig, $self, @args ) = @_;
+  my $config = $orig->( $self, @args );
+  my $own_config = {
+    normal_form => $self->normal_form,
+    mantissa    => $self->mantissa,
+  };
+  $config->{ '' . __PACKAGE__ } = $own_config;
+  return $config;
 };
 
 no Moose::Role;
---
+++ t/03_number_to_3.t
@@ -0,0 +1,35 @@
+use strict;
+use warnings;
+
+use Test::More;
+use Path::FindDev qw( find_dev );
+use Path::Tiny;
+use Cwd qw( cwd );
+use File::Copy::Recursive qw( rcopy );
+
+my $dist    = 'fake_dist_03';
+my $source  = find_dev('./')->child('corpus')->child($dist);
+my $tempdir = Path::Tiny->tempdir;
+
+rcopy( "$source", "$tempdir" );
+
+my $dist_ini = $tempdir->child('dist.ini');
+BAIL_OUT("test setup failed to copy to tempdir") if not -e $dist_ini and -f $dist_ini;
+
+use Test::Fatal;
+use Test::DZil;
+
+my $builder;
+
+is(
+  exception {
+    $builder = Builder->from_config( { dist_root => "$tempdir" } );
+    $builder->build;
+  },
+  undef,
+  "dzil build ran ok"
+);
+is( $builder->version, '1.200.300', 'Version is forced to prefixedless dotted decimal with 3 parts' );
+
+done_testing;
+
