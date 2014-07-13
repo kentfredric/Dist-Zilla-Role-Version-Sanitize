c25918e9a27fe5582fe20bb7a1215fcfdf69f1f1
Kent Fredric <kentfredric@gmail.com>
2014-07-14 09:42:20 +1200

    Build results of 8f2ef9f (on master)

--- lib/Dist/Zilla/Role/Version/Sanitize.pm
+++ lib/Dist/Zilla/Role/Version/Sanitize.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Role::Version::Sanitize;
 
-our $VERSION = '0.002000';
+our $VERSION = '0.002001';
 
 # ABSTRACT: Sanitize a version from a plugin
 
@@ -194,7 +194,7 @@ Dist::Zilla::Role::Version::Sanitize - Sanitize a version from a plugin
 
 =head1 VERSION
 
-version 0.002000
+version 0.002001
 
 =head1 ATTRIBUTES
 
