1796761914704014c09d54a6e1d4a7db64ae4888
Kent Fredric <kentfredric@gmail.com>
2017-03-03 22:10:03 +1300

    Build results of 2485d2c (on master)

--- lib/Dist/Zilla/Role/Version/Sanitize.pm
+++ lib/Dist/Zilla/Role/Version/Sanitize.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Role::Version::Sanitize;
 
-our $VERSION = '0.002003';
+our $VERSION = '0.002004';
 
 # ABSTRACT: Sanitize a version from a plugin
 
@@ -194,7 +194,7 @@ Dist::Zilla::Role::Version::Sanitize - Sanitize a version from a plugin
 
 =head1 VERSION
 
-version 0.002003
+version 0.002004
 
 =head1 ATTRIBUTES
 
